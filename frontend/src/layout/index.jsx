c7e7471d6ebf54ffb5f2da3aa6d24797b3fc1484
UdayShankar3953 <139896347+UdayShankar3953@users.noreply.github.com>
2024-10-01 18:39:05 +0530

    Update index.jsx

--- frontend/src/layout/index.jsx
+++ frontend/src/layout/index.jsx
@@ -3,3 +3,4 @@ export { default as ErpLayout } from './ErpLayout';
 export { default as DefaultLayout } from './DefaultLayout';
 export { default as DashboardLayout } from './DashboardLayout';
 export { default as SettingsLayout } from './SettingsLayout';
+
